8d665d695eeea7b9020c5623ca9ee5ea63d70fe4
AUGVV <68101056+AUGVV@users.noreply.github.com>
2021-09-13 18:42:38 +0300

    fix4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -14,18 +14,8 @@ namespace Task4
     public class Program
     {
         public static void Main(string[] args)
-        {
-            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
-            var host = new WebHostBuilder()
-              .UseKestrel()
-              .UseContentRoot(Directory.GetCurrentDirectory())
-              .UseConfiguration(config)
-              .UseIISIntegration()
-              .UseStartup<Startup>()
-              .Build();
-
-            host.Run();
-           // CreateHostBuilder(args).Build().Run();
+        {      
+           CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
